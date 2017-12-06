f737d3f8f1bf4829a7c1e2c61e60a5d7c78457e0
陈钦辉 <chenqinhui@didichuxing.com>
2017-12-06 21:26:16 +0800

    @dev udapte

--- template/webpack/webpack.base.conf.js
+++ template/webpack/webpack.base.conf.js
@@ -34,13 +34,13 @@ module.exports = {
     rules: [
       {{#lint}}
       {
-          test: /\.(js|vue)$/,
-            loader: 'eslint-loader',
-          enforce: 'pre',
-          include: [resolve('src'), resolve('test')],
-          options: {
-            formatter: require('eslint-friendly-formatter')
-          }
+        test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
       },
       {{/lint}}
       {
