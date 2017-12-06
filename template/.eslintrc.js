d72d4f8ed4ad5653ceda19a2bd2037532d79bf52
陈钦辉 <chenqinhui@didichuxing.com>
2017-12-06 22:18:06 +0800

    @dev one-var lint

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -31,6 +31,8 @@ module.exports = {
     'generator-star-spacing': 0,
     {{/if_eq}}
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // off
+    'one-var': 0
   }
 }
